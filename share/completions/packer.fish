07181b293978811edcae6f019167a30fc8816e19
Ryuhei Yoshida <yoshida.ryuhei0112@gmail.com>
2023-11-26 23:54:07 +0900

    Add instruction for packer auto-completion

---
+++ share/completions/packer.fish
@@ -0,0 +1,2 @@
+# Completions for packer are maintained upstream.
+# They should be installed with packer, please follow the instruction at https://developer.hashicorp.com/packer/docs/commands#autocompletion
